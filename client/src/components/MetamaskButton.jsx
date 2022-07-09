8947d9b6f8e398752453a7a6bf4d96a092b6ae43
DecentraMindsCoin <decentraminds@gmail.com>
2022-07-08 22:53:27 -0700

    Create a reusable metmask login button component with context state

---
+++ client/src/components/MetamaskButton.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { TransactionContext } from "../context/TransactionContext";
+import { AiFillPlayCircle } from "react-icons/ai";
+const MetamaskButton = () => {
+  const { currentAccount, connectWallet } = useContext(TransactionContext);
+  return (
+    <div className="" >
+      {!currentAccount && (
+        <button
+          type="button"
+          onClick={connectWallet}
+          className="flex flex-row justify-center items-center my-5 bg-gradient-to-r from-purple-500 to-yellow-500 hover:from-yellow-500 hover:to-purple-500 p-3 rounded-full cursor-pointer "
+        >
+          <AiFillPlayCircle className="text-white mr-2" />
+          <p className="text-white text-base font-semibold">Connect Wallet</p>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default MetamaskButton;
