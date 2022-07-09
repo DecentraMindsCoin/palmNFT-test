7d092d5b6a5947f6e8e59cf74e4d0ae160ea9ab9
DecentraMindsCoin <decentraminds@gmail.com>
2022-07-08 23:01:33 -0700

    add conditional rendered Gif that will show a static link if dynamic GIF can not be rendered && add span tag around addresses to show hyperlink capabiity

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -6,27 +6,48 @@ import useFetch from "../hooks/useFetch";
 import dummyData from "../utils/dummyData";
 import { shortenAddress } from "../utils/shortenAddress";
 
-const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+const TransactionsCard = ({
+  addressTo,
+  addressFrom,
+  timestamp,
+  message,
+  keyword,
+  amount,
+  url,
+}) => {
   const gifUrl = useFetch({ keyword });
 
   return (
-    <div className="bg-[#181918] m-4 flex flex-1
+    <div
+      className="bg-[#181918] m-4 flex flex-1
       2xl:min-w-[450px]
       2xl:max-w-[500px]
       sm:min-w-[270px]
       sm:max-w-[300px]
-      min-w-full
-      flex-col p-3 rounded-md hover:shadow-2xl"
+      
+      flex-col p-3 rounded-md border-2"
     >
-      <div className="flex flex-col items-center w-full mt-3">
+      <div className="flex flex-col items-center w-full mt-3 ">
         <div className="display-flex justify-start w-full mb-6 p-2">
-          <a href={`https://explorer.palm-uat.xyz/address/${addressFrom}`} target="_blank" rel="noreferrer">
-            <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
+          <a
+            href={`https://explorer.palm-uat.xyz/address/${addressFrom}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="text-yellow-300 hover:text-purple-300 text-base">
+             <span className="font-bold text-white">From: </span> {shortenAddress(addressFrom)}
+            </p>
           </a>
-          <a href={`https://explorer.palm-uat.xyz/address/${addressTo}`} target="_blank" rel="noreferrer">
-            <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
+          <a
+            href={`https://explorer.palm-uat.xyz/address/${addressTo}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="text-yellow-300 hover:text-purple-300 text-base">
+              <span className="font-bold text-white">To:</span>  {shortenAddress(addressTo)}
+            </p>
           </a>
-          <p className="text-white text-base">Amount: {amount} PALM</p>
+          <p className="text-white text-base"> <span className="font-bold">Amount:</span>  {amount} PALM</p>
           {message && (
             <>
               <br />
@@ -35,12 +56,17 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
           )}
         </div>
         <img
-          src={gifUrl || url}
+          src={
+            gifUrl
+              ? gifUrl
+              : url ||
+                "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284"
+          }
           alt="nature"
           className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
         />
         <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-          <p className="text-[#37c7da] font-bold">{timestamp}</p>
+          <p className="text-purple-300 font-bold">{timestamp}</p>
         </div>
       </div>
     </div>
@@ -63,7 +89,7 @@ const Transactions = () => {
           </h3>
         )}
 
-        <div className="flex flex-wrap justify-center items-center mt-10">
+        <div className="flex flex-wrap justify-center items-center mt-10 ">
           {[...transactions].reverse().map((transaction, i) => (
             <TransactionsCard key={i} {...transaction} />
           ))}
