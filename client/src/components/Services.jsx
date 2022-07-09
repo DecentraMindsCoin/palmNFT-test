0dd17f0f2179c9e1be8862902d428bb4b33d449b
DecentraMindsCoin <decentraminds@gmail.com>
2022-07-08 23:05:16 -0700

    import Palm.io main page services and features to brand keep application congruent with palm.io

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -3,6 +3,7 @@ import { BsShieldFillCheck } from "react-icons/bs";
 import { BiSearchAlt } from "react-icons/bi";
 import { RiHeart2Fill } from "react-icons/ri";
 
+
 const ServiceCard = ({ color, title, icon, subtitle }) => (
   <div className="flex flex-row justify-start items-start white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
     <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
@@ -35,21 +36,25 @@ const Services = () => (
       <div className="flex-1 flex flex-col justify-start items-center">
         <ServiceCard
           color="bg-[#2952E3]"
-          title="Security gurantee"
+          title="Scalable and
+          Sustainable"
           icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
-          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+          subtitle="Together, with some of the biggest names in fine art, cryptoart, and entertainment, we are building a new creative studio and NFT platform on Ethereum that is both scalable and sustainable."
         />
         <ServiceCard
           color="bg-[#8945F8]"
-          title="Best exchange rates"
+          title="Invested in
+          Creativity"
           icon={<BiSearchAlt fontSize={21} className="text-white" />}
-          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+          subtitle="Palm NFT Studio brings projects and platforms into the Palm ecosystem by supporting and collaborating with creatives, artists, marketplaces, and rights holders."
         />
+
         <ServiceCard
           color="bg-[#F84550]"
-          title="Fastest transactions"
+          title="Low Gas,
+          Shared Growth"
           icon={<RiHeart2Fill fontSize={21} className="text-white" />}
-          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+          subtitle="Creators using the Palm Network will benefit from super low gas fees, and the Palm ecosystem will reward both creators and participants."
         />
       </div>
     </div>
