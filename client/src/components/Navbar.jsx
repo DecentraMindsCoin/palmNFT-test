7f06da0933d36cbf3f777c06095a2b52d9789f88
DecentraMindsCoin <decentraminds@gmail.com>
2022-07-09 23:54:12 -0700

    add new routed links titles mapped with custom external links, change flex styling and connect button position to center

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,10 +4,30 @@ import { AiOutlineClose } from "react-icons/ai";
 
 import logo from "../../images/logo.png";
 import MetamaskButton from "./MetamaskButton";
+const NavLinks = [
+  {
+    linkName: "About",
+    link: "https://palm.io/",
+  },
+  {
+    linkName: "Get PALM",
+    link: "https://docs.palm.io/Get-Started/Tokens/",
+  },
 
-const NavBarItem = ({ title, classprops }) => (
-  <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
-);
+  {
+    linkName: "Explorer",
+    link: "https://explorer.palm-uat.xyz/",
+  },
+  {
+    linkName: "Developers",
+    link: "https://docs.palm.io/HowTo/Develop-For-Palm/",
+  },
+
+  {
+    linkName: "Blog",
+    link: "https://palm.io/studio/",
+  },
+];
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -18,9 +38,22 @@ const Navbar = () => {
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
-          <NavBarItem key={item + index} title={item} />
-        ))}
+        <div className="md:flex w-full pr-5">
+          {NavLinks.map((nav, index) => (
+            <div
+              key={index}
+              className="cursor-pointer border-b border-white lg:border-b-0"
+            >
+              <a href={nav.link}>
+                <div className="lg:inline-flex lg:w-auto px-2  py-4 active:bg-purple-500  hover:bg-button-image-2 rounded-xl text-gray-200 font-bold items-center  justify-center  hover:text-gray-200 hover:scale-110 transition duration-200 transform ease-out">
+                  <h1 id="glitch" className="text-gray-200  text-center">
+                    {nav.linkName}
+                  </h1>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>{" "}
         <MetamaskButton />
       </ul>
       <div className="flex relative">
@@ -41,20 +74,27 @@ const Navbar = () => {
         {toggleMenu && (
           <ul
             className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+            flex flex-col justify-start items-center rounded-md blue-glassmorphism text-white animate-slide-in"
           >
             <li className="text-xl w-full my-2">
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
             </li>
-            {["Market", "Exchange", "Tutorials", "Wallets"].map(
-              (item, index) => (
-                <NavBarItem
-                  key={item + index}
-                  title={item}
-                  classprops="my-2 text-lg"
-                />
-              )
-            )}{" "}
+            <div className="lg:flex w-full ">
+              {NavLinks.map((nav, index) => (
+                <div
+                  key={index}
+                  className="cursor-pointer border-b border-white lg:border-b-0"
+                >
+                  <a href={nav.link}>
+                    <div className="lg:inline-flex lg:w-auto px-2 pr-5 py-4 active:bg-purple-500 hover:pr-16  hover:bg-button-image-2 rounded-xl text-gray-200 font-bold items-center  justify-center  hover:text-gray-200 hover:scale-110 transition duration-200 transform ease-out">
+                      <h1 id="glitch" className="text-gray-200  text-center">
+                        {nav.linkName}
+                      </h1>
+                    </div>
+                  </a>
+                </div>
+              ))}
+            </div>{" "}
             <MetamaskButton />
           </ul>
         )}
