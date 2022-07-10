0b327aebabb47211c2aecddda1daeca6f8065a3e
DecentraMindsCoin <decentraminds@gmail.com>
2022-07-09 23:55:23 -0700

    change button inner text from connect wallet to connect

--- client/src/components/MetamaskButton.jsx
+++ client/src/components/MetamaskButton.jsx
@@ -12,7 +12,7 @@ const MetamaskButton = () => {
           className="flex flex-row justify-center items-center my-5 bg-gradient-to-r from-purple-500 to-yellow-500 hover:from-yellow-500 hover:to-purple-500 p-3 rounded-full cursor-pointer "
         >
           <AiFillPlayCircle className="text-white mr-2" />
-          <p className="text-white text-base font-semibold">Connect Wallet</p>
+          <p className="text-white text-base font-semibold">Connect</p>
         </button>
       )}
     </div>
