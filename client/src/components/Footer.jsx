9fa6b4fb9a433d213c05f2e21188d3e0eaeb81f9
DecentraMindsCoin <decentraminds@gmail.com>
2022-07-10 00:42:42 -0700

    add mapped routed links to Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { NavLinks } from "./Navbar";
 import logo from "../../images/logo.png";
 
 const Footer = () => (
@@ -9,22 +9,32 @@ const Footer = () => (
         <img src={logo} alt="logo" className="w-32" />
       </div>
       <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Tutorials</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Wallets</p>
+      {NavLinks.map((nav, index) => (
+            <div
+              key={index}
+              className="cursor-pointer border-b border-white lg:border-b-0"
+            >
+              <a href={nav.link}>
+                <div className="lg:inline-flex lg:w-auto px-2  py-4 active:bg-purple-500  hover:bg-button-image-2 rounded-xl text-gray-200 font-bold items-center  justify-center  hover:text-gray-200 hover:scale-110 transition duration-200 transform ease-out">
+                  <h1 id="glitch" className="text-gray-200  text-center">
+                    {nav.linkName}
+                  </h1>
+                </div>
+              </a>
+            </div>
+          ))}
       </div>
     </div>
 
     <div className="flex justify-center items-center flex-col mt-5">
       <p className="text-white text-sm text-center uppercase">Join us to discover what all the hype is about!</p>
-      <p className="text-white text-sm text-center font-medium mt-2">palm.io</p>
+      <a className="text-white text-sm text-center font-medium mt-2 hover:text-yellow-300" href="https://palm.io">Visit Palm.io</a>
     </div>
 
     <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-      <p className="text-white text-left text-xs">@decentraminds</p>
+      <a className="text-white text-left text-xs" href="https://decentraminds.com">Built by @decentraminds</a>
       <p className="text-white text-right text-xs">All rights reserved</p>
     </div>
   </div>
