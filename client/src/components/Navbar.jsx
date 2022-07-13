ebd4e90f930b7064ed8d1981bdacc557822dd2ff
DecentraMindsCoin <decentraminds@gmail.com>
2022-07-12 23:04:08 -0700

    add target of _blank onClick to all navlinks that will open new page when clicked

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -120,7 +120,7 @@ const Navbar = () => {
                   key={index}
                   className="cursor-pointer border-b border-white lg:border-b-0"
                 >
-                  <a href={nav.link}>
+                  <a href={nav.link} target="_blank" rel="noreferrer">
                     <div className="lg:inline-flex lg:w-auto px-2 pr-5 py-4 active:bg-purple-500 hover:pr-5  hover:bg-button-image-2 rounded-xl text-gray-200 font-bold items-center  justify-center  hover:text-gray-200 hover:scale-110 transition duration-200 transform ease-out">
                       <h1 id="glitch" className="text-gray-200  text-center">
                         {nav.linkName}
